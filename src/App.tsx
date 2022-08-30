3df4128e1faec21357b566201249c88158b04449
Rodrigo Barros <brodrigo87@gmail.com>
2022-08-30 16:18:20 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { GlobalStyles } from './styles/globalstyles'
-import { Header } from './styles/styles'
+import { Header, Logo, HeaderMain, Tittle, Subtitle } from './styles/styles'
 import  Button  from './components/Button';
 
 function App() {
@@ -10,10 +10,24 @@ function App() {
       <GlobalStyles/>
 
       <Header>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+      <Logo>
+        <img src="/images/logo_mindmaster.webp" alt="" />
+      </Logo>
 
       </Header>
+
+      <HeaderMain>
+        <p>PENSAMENTO COMPUTACIONAL</p>
+        <Tittle>
+          MindMaster - Computational Thinking <span>.</span>
+        </Tittle>
+
+        <Subtitle>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+        </Subtitle>
+
+      </HeaderMain>
 
       
       
--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,10 +1,49 @@
 import styled from "styled-components";
 
 
+function pixelToRem(...values: number[]){
+    return values
+    .reduce((acc, current) =>(acc += current / 16 + `rem `), "")
+    .trim();
+}
+
+
 export const Header = styled.div`
 
     display: flex;
     background-color: var(--space);
-    // width: 100%
+    width: 100%
     font: var(--font-display);
+`;
+
+export const Logo = styled.image`
+
+    width: pixeltoRem(201);
+    height: pixeltoRem(41);
+
+
+`;
+
+export const HeaderMain = styled.div`
+
+    width: pixeltoRem(201);
+    height: pixeltoRem(41);
+
+
+`;
+
+export const Tittle = styled.div`
+
+    width: pixeltoRem(201);
+    height: pixeltoRem(41);
+
+
+`;
+
+export const Subtitle = styled.div`
+
+    width: pixeltoRem(201);
+    height: pixeltoRem(41);
+
+
 `;
